ed3283e6d6902b0149c52e7631bbd041e7ad3d6d
Kelly Hansen <kelly.hansen@hotbsoftware.com>
2022-12-13 20:56:52 -0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const $phoneNumber = document.querySelector('.phone-number');
+$phoneNumber.textContent = '(760) 605-3609';
+
+const $emailAddress = document.querySelector('.email-address');
+$emailAddress.textContent = 'Ceja.Rodrigo@gmail.com';
