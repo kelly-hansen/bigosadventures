bfebfa4d97b35309a33498b5787dd8e2229704e1
Kelly Hansen <kelly.hansen@hotbsoftware.com>
2022-12-28 09:28:57 -0800

    Add photos and styling cleanup

--- js/main.js
+++ js/main.js
@@ -2,4 +2,4 @@ const $phoneNumber = document.querySelector('.phone-number');
 $phoneNumber.textContent = '(760) 605-3609';
 
 const $emailAddress = document.querySelector('.email-address');
-$emailAddress.textContent = 'Ceja.Rodrigo@gmail.com';
+$emailAddress.textContent = 'BigosAdventures@gmail.com';
